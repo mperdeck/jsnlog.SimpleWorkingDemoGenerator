18c8a9bc834a04bafc9470e28b4ad9d268d8e5b0
Matt Perdeck <mperdeck@gmail.com>
2016-07-09 23:25:56 +1000

    testing

--- Generator/helpers/Sites.ps1
+++ Generator/helpers/Sites.ps1
@@ -3,6 +3,6 @@
 #
 
 $sites = @(
-	[pscustomobject]@{projectName="JSNLogDemo_Log4Net"; features=@("Base", "Log4Net") },
+	[pscustomobject]@{projectName="JSNLogDemo_Serilog"; features=@("Base", "Serilog") },
 	[pscustomobject]@{projectName="JSNLogDemo_Elmah"; features=@("Base", "Elmah") }
 )
