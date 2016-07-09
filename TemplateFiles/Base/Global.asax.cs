14da3a297abc1e0a104003ff89a78afabb9e484a
Matt Perdeck <mperdeck@gmail.com>
2016-07-09 23:25:07 +1000

    markers now have {{{ instead of {{

--- TemplateFiles/Base/Global.asax.cs
+++ TemplateFiles/Base/Global.asax.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-{{LoggingPackageSpecificUsing}}
+{{{LoggingPackageSpecificUsing}}}
 
-namespace {{Project}}
+namespace {{{Project}}}
 {
     public class MvcApplication : System.Web.HttpApplication
     {
